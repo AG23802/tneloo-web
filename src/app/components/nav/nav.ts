757491985d69f0709396b5d9111a75b251338b00
Rexhin Hoxha <rexhin@icloud.com>
2026-08-14 23:46:07 +0200

    refactor

--- src/app/components/nav/nav.ts
+++ src/app/components/nav/nav.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IconComponent } from '../icon/icon';
 import { UserService } from '../../core/services/user.service';
-
+import { IconName } from '../icon/icon.model';
 @Component({
   selector: 'app-nav',
   imports: [RouterLink, RouterLinkActive, IconComponent],
@@ -11,4 +11,40 @@ import { UserService } from '../../core/services/user.service';
 })
 export class Nav {
   userService = inject(UserService);
+
+  navItems: {
+    route: (username?: string) => string | string[];
+    icon: IconName;
+    activeIcon: IconName;
+    label: string;
+    exact?: boolean;
+    needsUsername?: boolean;
+  }[] = [
+    {
+      route: () => '/',
+      icon: 'home',
+      activeIcon: 'homeFilled',
+      label: 'Home',
+      exact: true,
+    },
+    {
+      route: () => '/search',
+      icon: 'search',
+      activeIcon: 'searchFilled',
+      label: 'Search',
+    },
+    {
+      route: () => '/chats',
+      icon: 'share',
+      activeIcon: 'shareFilled',
+      label: 'Chats',
+    },
+    {
+      route: (username) => (username ? ['/', username] : '/'),
+      icon: 'person',
+      activeIcon: 'personFilled',
+      label: 'Profile',
+      needsUsername: true,
+    },
+  ];
 }
